8e33c2f087f86cfbf7960c4973331588d349b287
KeynesYouDigit <vince.buscarello@gmail.com>
2025-03-28 15:20:04 -0600

    single header using utils idiom

--- public/community/KeynesYouDigIt/STAC_SIF_and_land/utils.py
+++ public/community/KeynesYouDigIt/STAC_SIF_and_land/utils.py
@@ -7,6 +7,10 @@ import requests
 import s3fs
 import xarray as xr
 
+import geopandas as gpd
+import stacrs
+from pystac import Item
+
 def _retrieve_nasa_credentials() -> dict:
     """Makes the Oauth calls to authenticate with EDS and return a set of s3
     same-region, read-only credntials.
@@ -121,3 +125,20 @@ def get_sif_data(bbox: list[int], overlap_buffer_size: float = 3.0) -> xr.Datase
     )
 
     return _detect_and_set_rio_data(sif_d_within_box)
+
+
+async def get_land_prod_data(year: int, bbox:gpd.GeoDataFrame) -> list[Item]:
+
+    # Find Items that intersect the bounding box and time period
+    item_dicts = await stacrs.search(
+        "https://data.ldn.auspatious.com/geo_ls_lp/geo_ls_lp_0_1_0.parquet",
+        bbox=bbox.total_bounds,
+        datetime=f"{year}-01-01T00:00:00.000Z/{year}-12-31T23:59:59.999Z",
+    )
+    items: list[Item] = [Item.from_dict(d) for d in item_dicts]
+
+    print(type(items))
+    print(len(items))
+
+
+    return items
